75c82be7cd16dca218719f5f6096590c0d80c804
malaramofficial <malaramofficial@gmail.com>
2025-11-24 04:20:17 +0530

    Update firebase API key fallback to empty string

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@
 export const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-2288106784-e0583",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:910474312216:web:87acb72078bb35265ae16d",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCwGYv_Ob3-M7Brw7s8_MmIkZxRIIdih_o",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-2288106784-e0583.firebaseapp.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "910474312216",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
