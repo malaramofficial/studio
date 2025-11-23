8383009fd0b1364ed5ba6dbfa7ebb4cc2bcb4c1f
Malaram Official <malaramofficial@gmail.com>
2025-11-23 19:08:18 +0000

    https://images.unsplash.com/photo-1763922065273-4a73139aa6d5?q=80&w=880&

--- src/components/creator/creator-profile.tsx
+++ src/components/creator/creator-profile.tsx
@@ -16,7 +16,7 @@ type CreatorProfileProps = {
 export function CreatorProfile({ creatorInfo }: CreatorProfileProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   
-  const creatorImageUrl = "https://i.ibb.co/gPdnC2s/IMG-20240321-220356.jpg";
+  const creatorImageUrl = "https://images.unsplash.com/photo-1763922065273-4a73139aa6d5?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
